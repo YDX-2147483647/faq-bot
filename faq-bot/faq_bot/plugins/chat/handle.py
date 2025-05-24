a0799db5250144dff84672031b819520e5e2b067
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-05-24 22:27:28 +0800

    feat: 改进`/chat`的输出格式

--- faq-bot/faq_bot/plugins/chat/handle.py
+++ faq-bot/faq_bot/plugins/chat/handle.py
@@ -16,6 +16,11 @@ from .config import Config
 
 config = get_plugin_config(Config).chat
 
+FOOTER = """
+═══ 提示 ═══
+有时询问通用大语言模型更好更快。实例可参考 https://bithesis.bitnp.net/guide/ask-computer.html 。
+""".strip()
+
 
 async def handle(message: str) -> str:
     """回复消息"""
@@ -72,25 +77,57 @@ async def handle(message: str) -> str:
 
     answer: str = response["MessageInfo"]["AnswerInfo"]["Answer"]
 
-    # 提取回答来源
     tracing: list[dict] = json.loads(
         response["MessageInfo"]["AnswerInfo"]["TracingJsonStr"]
     )
-    # docs[n] = (名称, URL或ID)；由于有知识库、问答库等多种类型，这里仅简单处理
-    docs: list[tuple[str, str]] = [
-        (
-            doc["metadata"]["document_name"] or doc["metadata"]["dataset_name"],
-            doc["metadata"]["document_url"] or doc["metadata"]["document_id"],
-        )
-        for doc in tracing[-1]["docs"]["outputList"]
-        # `tracing[-1]`即`tracing[1]`，是 knowledge_retrieve_end 事件
-    ]
 
     return "\n\n".join(
-        [
+        block
+        for block in [
             answer,
-            "═" * 6,
-            "回答来源",
-            *(f"{name}\n{url}" for name, url in docs),
+            humanize_tracing(tracing),
+            FOOTER,
         ]
+        if block is not None
     )
+
+
+def humanize_tracing(tracing: list[dict]) -> str | None:
+    """Humanize TracingJsonStr
+
+    通常包括回答来源，但也可能没有。
+    """
+
+    if (docs := tracing[-1].get("docs")) is not None:
+        # pairs[n] = (名称, URL/ID)；由于有知识库、问答库等多种类型，这里仅简单处理
+        pairs: list[tuple[str, str]] = [
+            (
+                doc["metadata"]["document_name"] or doc["metadata"]["dataset_name"],
+                doc["metadata"]["document_url"] or doc["metadata"]["document_id"],
+            )
+            for doc in docs["outputList"]
+            # `tracing[-1]`即`tracing[1]`，是 knowledge_retrieve_end 事件
+        ]
+
+        # 略去重复的 URL/ID
+        seen = set()
+        unique_pairs = []
+        for name, url in pairs:
+            if url not in seen:
+                unique_pairs.append((name, url))
+                seen.add(url)
+
+        # 挽回特殊情况
+        for i, (name, url) in enumerate(unique_pairs):
+            if not url.startswith("https") and name == "bithesis.pdf.txt":
+                unique_pairs[i] = (
+                    "bithesis.pdf",
+                    "https://texdoc.org/serve/bithesis/0",
+                )
+
+        return "\n".join(
+            [
+                "═══ 来源 ═══",
+                *(f"{name} — {url}" for name, url in unique_pairs),
+            ]
+        )
